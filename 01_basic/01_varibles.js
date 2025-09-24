56e8bf1bf1825f467acd1b00ec7c8e3d8d34cef5
Yash Rasal <yashrasal8973@gmail.com>
2025-09-24 13:33:31 +0000

    basic video1

---
+++ 01_basic/01_varibles.js
@@ -0,0 +1,14 @@
+const  accountId=12324
+let accountEmail="yash@goole.com"
+var accountPassword="12345"
+accountCity="Ratnagiri"
+let accountState;
+// accountId=2
+
+// console.log(accountId)
+
+accountEmail="yr@gmail.com"
+accountPassword="21212121"
+accountCity="Satara"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
