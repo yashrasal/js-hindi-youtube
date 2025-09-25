d1295a3f0a9791d74ba294c5ecd1df5f41f1c6ee
Yash Rasal <yashrasal8973@gmail.com>
2025-09-25 00:30:43 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score="33abc";
+// console.log(score)
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+let isLoggedIn="Yash";
+let booleanisLoggedIN=Boolean(isLoggedIn);
+
+// console.log(booleanisLoggedIN)
+let someNumber=33
+let someString=String(someNumber)
+console.log(someString)
+console.log(typeof someString)
