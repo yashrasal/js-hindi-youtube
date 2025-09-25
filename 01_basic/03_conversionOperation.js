5d70b31d3bac0c4a2fc9a7d1bd868989187b936b
Yash Rasal <yashrasal8973@gmail.com>
2025-09-25 00:50:29 +0000

    .

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -16,5 +16,23 @@ let booleanisLoggedIN=Boolean(isLoggedIn);
 // console.log(booleanisLoggedIN)
 let someNumber=33
 let someString=String(someNumber)
-console.log(someString)
-console.log(typeof someString)
+// console.log(someString)
+// console.log(typeof someString)
+
+//*********************opration**************************
+
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+let str1="hello"
+let str2=" Yash"
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+
+console.log("1"+2+2)
+console.log(1+2+"3")
