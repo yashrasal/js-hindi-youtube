de7a27c4fc82978646743b1c12e387117f060065
Yash Rasal <yashrasal8973@gmail.com>
2025-09-25 03:00:25 +0000

    .

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,15 @@
+let myYoutubeName="yashrasal.com"
+let anothername=myYoutubeName
+
+anothername="secondchanel"
+console.log(anothername)
+
+let userOne={
+    name:"Yash Rasal",
+    Email:"yashrasal@gmail.com"
+}
+
+let userTwo= userOne
+    userTwo.Email="Raj@gmail.com"
+    console.log(userOne)
+    console.log(userTwo)
