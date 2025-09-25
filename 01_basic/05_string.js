91bbd34c65c696c6efe41db389eed54bb489c678
Yash Rasal <yashrasal8973@gmail.com>
2025-09-25 13:28:28 +0000

    string

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -1,4 +1,32 @@
 let name="yash"
 let repo=50
 
-console.log(`Hellow my name is ${name}. my repocount is ${repo}`);
+// console.log(`Hellow my name is ${name}. my repocount is ${repo}`);
+
+let gameName=String('Yash-yr')
+
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase())
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf('h'))
+
+let newString=gameName.substring(0,4)
+// console.log(newString)
+
+
+const str="YashRasal"
+// console.log(str.slice(-7,5))
+
+
+const str2="    Yash   "
+
+// console.log(str2)
+// console.log(str2.trim())
+
+let url= "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('hitesh'))
+console.log(url.includes('Sunder'))
+
+console.log(gameName.split('-'))
